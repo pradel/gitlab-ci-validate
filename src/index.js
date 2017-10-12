be29d64c5ce87eea98bf8ab37c80823b60584ea7
Pradel Léo <pradel.leo@gmail.com>
2017-10-12 11:15:53 +0200

    use node-fetch instead of request

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
-const rp = require('request-promise');
+
+if (!process.browser) {
+  // eslint-disable-next-line global-require
+  global.fetch = require('node-fetch');
+}
 
 module.exports = function gitlabCiValidate(filePath) {
   const file = fs.readFileSync(filePath, 'utf8');
-  return rp({
+  // eslint-disable-next-line no-undef
+  return fetch('https://gitlab.com/api/v4/ci/lint', {
     method: 'POST',
-    uri: 'https://gitlab.com/api/v4/ci/lint',
     json: true,
-    body: {
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
       content: file,
-    },
-  });
+    }),
+  }).then(res => res.json());
 };
