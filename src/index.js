f6b2f6fff7f364df2263ac5915118de1b8d893ac
marcelomarra <marcelo@devsquad.email>
2019-07-08 17:26:31 -0300

    Fixing lint errors

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
+const fs = require('fs')
 
 if (!process.browser) {
   // eslint-disable-next-line global-require
-  global.fetch = require('node-fetch');
+  global.fetch = require('node-fetch')
 }
 
 module.exports = function gitlabCiValidate(filePath, options = {}) {
   if (!options.host) {
-    options.host = 'https://gitlab.com';
+    options.host = 'https://gitlab.com'
   }
-  const file = fs.readFileSync(filePath, 'utf8');
-  let headers = { 'Content-Type': 'application/json' };
+  const file = fs.readFileSync(filePath, 'utf8')
+  let headers = { 'Content-Type': 'application/json' }
   if (options.private_token) {
-    headers['Private-Token'] = options.private_token;
+    headers['Private-Token'] = options.private_token
   }
   // eslint-disable-next-line no-undef
   return fetch(`${options.host}/api/v4/ci/lint`, {
@@ -20,7 +20,7 @@ module.exports = function gitlabCiValidate(filePath, options = {}) {
     json: true,
     headers: headers,
     body: JSON.stringify({
-      content: file,
-    }),
-  }).then(res => res.json());
-};
+      content: file
+    })
+  }).then(res => res.json())
+}
