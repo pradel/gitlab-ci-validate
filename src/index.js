916ecd8433f2b1ead9b7604dd62434c2953cbf0e
marcelomarra <4422390+marcelomarra@users.noreply.github.com>
2019-07-14 06:51:48 -0300

    Adding private token argument. (#4)
    * Adding (optional) private token argument. Catching different errors.
    
    * Adding system.exit and changing option name

--- src/index.js
+++ src/index.js
@@ -10,11 +10,15 @@ module.exports = function gitlabCiValidate(filePath, options = {}) {
     options.host = 'https://gitlab.com';
   }
   const file = fs.readFileSync(filePath, 'utf8');
+  let headers = { 'Content-Type': 'application/json' };
+  if (options.private_token) {
+    headers['Private-Token'] = options.private_token;
+  }
   // eslint-disable-next-line no-undef
   return fetch(`${options.host}/api/v4/ci/lint`, {
     method: 'POST',
     json: true,
-    headers: { 'Content-Type': 'application/json' },
+    headers: headers,
     body: JSON.stringify({
       content: file,
     }),
